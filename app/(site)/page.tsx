ea527830f57332b814333ae7e67e9d8bf784286b
wisdompandamaster <2532569059@qq.com>
2023-08-31 23:16:47 +0800

    feat(site):init the start page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-sky-500 text-center font-semibold text-2xl m-10">
+      Cheems
+    </h1>
+  );
+}
