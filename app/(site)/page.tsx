736309e18599b6b479da5c15f5e7f48bf7fac923
wisdompandamaster <2532569059@qq.com>
2023-09-01 18:52:21 +0800

    feat(page):add a bg for test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-sky-500 text-center font-semibold text-2xl m-10">
-      Cheems
-    </h1>
+    <div>
+      <h1 className="text-sky-500 text-center font-semibold text-2xl p-10">
+        Cheems
+      </h1>
+    </div>
   );
 }
