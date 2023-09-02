cb9b169120af613bce3f0289e170593dda7f8f74
wisdompandamaster <2532569059@qq.com>
2023-09-02 12:20:36 +0800

    feat(Altar):start the layout

---
+++ app/(site)/components/Altar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Altar = () => {
+  return (
+    <div className="flex justify-center w-full h-full">
+      <div className="w-1/3 h-2/5 bg-gray-500 flex justify-center items-center">
+        Picture Here
+      </div>
+    </div>
+  );
+};
+
+export default Altar;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Altar from "./components/Altar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-full">
       <h1 className="text-sky-500 text-center font-semibold text-2xl p-10">
         Cheems
       </h1>
+      <Altar />
     </div>
   );
 }
