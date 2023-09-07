ffab1833e37f6040941981d0b7b59b1d0a19c90e
wisdompandamaster <2532569059@qq.com>
2023-09-07 19:39:21 +0800

    feat(BulletScreen):get the random Duration

--- app/(site)/components/BulletScreen.tsx
+++ app/(site)/components/BulletScreen.tsx
@@ -2,8 +2,9 @@
 
 import clsx from "clsx";
 import { randomBytes } from "crypto";
-import { useState, useEffect } from "react";
+import { useState, useEffect, TimeHTMLAttributes } from "react";
 import Bullet from "./Bullet";
+import { MdOutlineCommentsDisabled, MdOutlineComment } from "react-icons/md";
 
 const Barrage = () => {
   let init = [
@@ -22,6 +23,7 @@ const Barrage = () => {
   ];
 
   const [barrages, setBarrages] = useState(init);
+  const [showWall, setShowWall] = useState(true);
 
   // 随机出现在哪一行 0% 到 90%  6% 为一级,一共 15 行
   const randomTop = () => {
@@ -30,7 +32,7 @@ const Barrage = () => {
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber * 6 + "%";
   };
-  // 随机持续时间
+  // 随机持续时间,使每条弹幕移动速度不同
   const randomDuration = () => {
     return Math.floor(Math.random() * 6 + 20) + "s";
   };
@@ -59,7 +61,8 @@ const Barrage = () => {
   useEffect(() => {
     const wall = document.getElementById("wall");
     // 每两秒取一个弹幕显示
-    setInterval(() => {
+    // TODO: find a way to clearinterval
+    let timer = setInterval(() => {
       var randomIndex = Math.floor(Math.random() * barrages.length);
       let barrageContent = barrages[randomIndex];
       const bullet = document.createElement("div");
@@ -73,6 +76,7 @@ const Barrage = () => {
         `animate-[flow_20s_linear]`
       );
       bullet.style.top = randomTop();
+      bullet.style.animationDuration = randomDuration();
       bullet.textContent = barrageContent.content;
       // 动画结束后去除元素
       bullet.addEventListener("animationend", (e) => {
@@ -81,13 +85,58 @@ const Barrage = () => {
       });
       wall?.appendChild(bullet);
     }, 1000);
-  }, [barrages]);
+
+    return () => {
+      clearInterval(timer);
+      console.log("unmount barrage");
+      console.log("clearInterval");
+    };
+  }, [barrages, showWall]);
 
   return (
-    <div
-      id="wall"
-      className="w-full h-2/5 bg-black bg-opacity-40 absolute overflow-x-hidden z-50"
-    ></div>
+    <>
+      <div
+        id="wall"
+        className={clsx(
+          ` 
+             w-full 
+             h-2/5 
+           bg-black 
+             bg-opacity-40 
+             absolute 
+             overflow-x-hidden 
+             z-50
+            `,
+          showWall && "hidden"
+        )}
+      ></div>
+      <div
+        className="
+        fixed
+        bottom-8
+        right-28
+        w-14
+        h-14
+        rounded-lg
+        bg-white
+        bg-opacity-60
+        p-3
+        flex
+        items-center
+        select-none
+        hover:bg-opacity-80
+        duration-300
+      "
+      >
+        <div onClick={() => setShowWall(!showWall)}>
+          {showWall ? (
+            <MdOutlineCommentsDisabled size={30} />
+          ) : (
+            <MdOutlineComment size={30} />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
